47ff298e373d8147aa9b65b96b4855e5b9e066fe
AnnWann <uanabgasparim@gmail.com>
2025-02-07 14:05:38 -0300

    feature/src/routes/serverRoutes/->serverRoutes INCOMPLETE

---
+++ src/routes/serverRoutes/serverRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+
+export default (router: Router) => {
+  router.put("/View_Change", (req, res) => {
+    
+  });
+  router.put("/VC_Proof", (req, res) => {
+  
+  });
+  router.put("/Prepare", (req, res) => {
+  
+  });
+  router.put("/Prepare_OK", (req, res) => {
+  
+  });
+  router.put("/Proposal", (req, res) => {
+  
+  });
+  router.put("/Accept", (req, res) => {
+  
+  });
+}
