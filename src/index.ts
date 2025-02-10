52c4320ff135d0496212ba543e1984f1de941938
AnnWann <uanabgasparim@gmail.com>
2025-02-09 23:14:50 -0300

    feature/src/routes/clientRoutes/->clientRoutes.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 require("dotenv").config();
+import { Decision_Starter } from "decision_starter/Decision_Starter";
 import { SERVER } from "./server/server";
 
-const PORT = process.env.PORT;
+const PORT = process.env.MY_PORT
 SERVER.GET_INSTANCE().setPort(Number(PORT)).run().getServer();
+
+// inicia decision_starter
+
+Decision_Starter.__GET__()
 
 
